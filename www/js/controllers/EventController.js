434cd8e5e7f475088098880cafc3f36c8f8084ba
Akhil Asaad <aquiles19939@gmail.com>
2017-09-07 18:09:05 -0300

    update

--- www/js/controllers/EventController.js
+++ www/js/controllers/EventController.js
@@ -4,32 +4,52 @@ angular.module('PsiPlannerApp')
 
 .controller('EventController', EventController);
 
-EventController.$inject = ['$scope'];
+EventController.$inject = ['$scope', '$ionicLoading', 'SubjectsService', 'EventsService'];
 
-function EventController($scope) {
-    $scope.event = {
-        subject_id: null,
-        title: null,
-        description: null,
-        color: null,
-        date_start: null,
-        date_end: null,
-        alert_date: null
-    };
+function EventController($scope, $ionicLoading, SubjectsService, EventsService) {
+    document.addEventListener("deviceready", function () {
 
-    $scope.mySubjects = [];
-    $scope.colors = [
-        {id: 'positive', name: 'Azul'},
-        {id: 'calm', name: 'Celeste'},
-        {id: 'balanced', name: 'Verde'},
-        {id: 'energized', name: 'Amarillo'},
-        {id: 'assertive', name: 'Rojo'},
-        {id: 'royal', name: 'Violeta'},
-        {id: 'dark', name: 'Negro'}
-    ];
+        $scope.colors = [
+            {id: 'positive', name: 'Azul'},
+            {id: 'calm', name: 'Celeste'},
+            {id: 'balanced', name: 'Verde'},
+            {id: 'energized', name: 'Amarillo'},
+            {id: 'assertive', name: 'Rojo'},
+            {id: 'royal', name: 'Violeta'},
+            {id: 'dark', name: 'Negro'}
+        ];
 
-    $scope.addEvent = function() {
-        console.log('Hola');
-    };
+        $scope.addEvent = function() {
+            console.log($scope.event);
+        };
+
+        $ionicLoading.show({
+            content: 'Loading',
+            showBackdrop: true,
+            maxWidth: 200,
+            showDelay: 0
+        });
+
+        SubjectsService.getMySubjects()
+        .then(function(mySubjects) {
+            $scope.mySubjects = mySubjects;
+            $scope.event = {
+                subject_id: null,
+                title: null,
+                description: null,
+                color: null,
+                date_start: null,
+                date_end: null,
+                alert_date: null
+            };
+        })
+        .catch(function(error) {
+            console.error(error);
+        })
+        .finally(function() {
+            $ionicLoading.hide();
+            console.log($scope.event.subject_id);
+        })
+    });
 
 }
