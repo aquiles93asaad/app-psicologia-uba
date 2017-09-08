71fcfa2ee2e8ff657d02a65d29015db2aeeef9dc
Akhil Asaad <aquiles19939@gmail.com>
2017-09-08 20:46:14 -0300

    update

--- www/js/controllers/MySubjectController.js
+++ www/js/controllers/MySubjectController.js
@@ -4,8 +4,93 @@ angular.module('PsiPlannerApp')
 
 .controller('MySubjectController', MySubjectController);
 
-MySubjectController.$inject = ['$scope'];
+MySubjectController.$inject = [
+    '$scope',
+    '$state',
+    '$cordovaToast',
+    '$ionicHistory',
+    '$ionicLoading',
+    'SubjectsService',
+    'DateTransformerService'
+];
 
-function MySubjectController($scope) {
+function MySubjectController(
+    $scope,
+    $state,
+    $cordovaToast,
+    $ionicHistory,
+    $ionicLoading,
+    SubjectsService,
+    DateTransformerService
+) {
 
+    $scope.stateOptions = [
+        {id: 'Cursando'},
+        {id: 'Aprobada'},
+        {id: 'Debe final'},
+        {id: 'Recursada'}
+    ];
+
+    document.addEventListener("deviceready", function () {
+        $ionicLoading.show({
+            content: 'Loading',
+            showBackdrop: true,
+            maxWidth: 200,
+            showDelay: 0
+        });
+
+        SubjectsService.getById($state.params.subjectId)
+        .then(function(subject) {
+            $scope.subject = subject;
+            return SubjectsService.getClasses($scope.subject.id);
+        })
+        .then(function(classes) {
+            $scope.classes = classes;
+
+            if(!$scope.subject.current_class_id) {
+                $scope.subject.current_class_id = $scope.classes[0];
+            } else {
+                angular.forEach($scope.classes, function(clase, key) {
+                    if(clase.id == $scope.subject.current_class_id) {
+                        $scope.subject.current_class_id = clase;
+                    }
+                });
+            }
+
+            return SubjectsService.getPreCorrelatives($scope.subject.id);
+        })
+        .then(function(preCorrelatives) {
+            $scope.preCorrelatives = preCorrelatives;
+            return SubjectsService.getPostCorrelatives($scope.subject.id);
+        })
+        .then(function(postCorrelatives) {
+            $scope.postCorrelatives = postCorrelatives;
+        })
+        .catch(function(error) {
+            console.error(error);
+        })
+        .finally(function() {
+            $ionicLoading.hide();
+        });
+    });
+
+    $scope.updateSubject = function() {
+        var subjectToAdd = {
+            state: $scope.subject.state,
+            current_class_id: $scope.subject.current_class_id.id
+        };
+
+        SubjectsService.addSubject(parseInt($scope.subject.id), subjectToAdd)
+        .then(function(success) {
+            $cordovaToast.showShortBottom("La materia se agregó a Mi Carrera!");
+
+            $ionicHistory.nextViewOptions({
+                disableBack: true
+            });
+            $state.go('app.mySubjects');
+        })
+        .catch(function(error) {
+            console.error(error);
+        })
+    };
 }
